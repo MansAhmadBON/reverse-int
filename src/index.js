d28623bfea387eee2a261101bb3b84a0a82fde7b
MansAhmadBONgit config --global user.name MansAhmadBONgit config --global user.name MansAhmadBON <mansue.ahmad.94@mail.ru>
2020-02-20 15:02:00 +0300

    feat: added answer

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    let strN = "" + n;
+    let newStr = strN.replace(/\-/g, "");
+    return Number(
+        newStr
+            .split("")
+            .map(e => Number(e))
+            .reverse()
+            .join("")
+    );
+};
